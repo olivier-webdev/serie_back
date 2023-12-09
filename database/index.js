8c73385a7104bcaf21fe1a0fa668784e13facbcb
olivier-webdev <olivier.webdev@gmail.com>
2023-12-09 13:31:12 +0100

    try to fix unknown pool variable

--- database/index.js
+++ database/index.js
@@ -8,7 +8,18 @@ require("dotenv").config();
 //   database: "eval_serie",
 // });
 
-connection = mysql.createConnection({
+// connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DB,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
